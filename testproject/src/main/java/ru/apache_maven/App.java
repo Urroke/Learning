709fbd0a8b9bfe2d8a9606ffda8577fd765d033b
Urroke <kovalevyura1@gmail.com>
2019-07-18 14:04:01 +0600

    first commit

---
+++ testproject/src/main/java/ru/apache_maven/App.java
@@ -0,0 +1,46 @@
+package ru.apache_maven;
+
+class A{
+    private int a;
+    private int b;
+
+    public A(){;}
+
+    public A(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void Print()
+    {
+        System.out.print(a + "\t" + b + "\n");
+    }
+}
+
+class B extends A{
+    private int c;
+
+    public B(){;}
+
+    public B(int a, int b, int c)
+    {
+        super(a, b);
+        this.c = c;
+    }
+
+    public void Print() {
+        super.Print();
+        System.out.println("\b\t" + c + "\n");
+    }
+}
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        A a = new A(1, 2);
+        B b = new B(2, 3, 4);
+        b.Print();
+    }
+}
